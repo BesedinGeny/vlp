7778f317f4235bdf21ac3b5b2c1756fd6c69e30a
vvnumb <besedin_geny@mail.ru>
2023-01-23 16:44:00 +0700

    fix(vlp): уменьшено тело выхова функции:
    -
    
    Решено: -

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -9,14 +9,5 @@ main_router = APIRouter(prefix="/vlp", tags=["VLP"])
 def calc_vlp(vlp_in: VlpCalcRequest):
     """Расчёт VLP по исходным данным и сохранение в Базу"""
     from src.calculations.vlp import calc_vlp as vlp_calculation  # noqa
-    data = dict(
-        inclinometry=vlp_in.inclinometry.dict(),
-        casing=vlp_in.casing.dict(),
-        tubing=vlp_in.tubing.dict(),
-        pvt=vlp_in.pvt.dict(),
-        p_wh=vlp_in.p_wh,
-        geo_grad=vlp_in.geo_grad,
-        h_res=vlp_in.h_res
-    )
-    return vlp_calculation(**data)
+    return vlp_calculation(**vlp_in.dict())
 
